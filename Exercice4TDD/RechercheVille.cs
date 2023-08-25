ec5b3f3c0486299c748a58f2abf05c92182ffc42
TOPART62 <101316923+TOPART62@users.noreply.github.com>
2023-08-25 13:42:19 +0200

    Init Class RechercheVille

---
+++ Exercice4TDD/RechercheVille.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice4TDD
+{
+    internal class RechercheVille
+    {
+        private List<String> _villes;
+
+        public List<String> Rechercher(String mot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
