dfc899c14d24bc596733576e00610c6027088bf7
TOPART62 <101316923+TOPART62@users.noreply.github.com>
2023-08-25 14:22:19 +0200

    Ajout Class NotFoudException

--- Exercice4TDD.MsTest/RechercheVilleTest.cs
+++ Exercice4TDD.MsTest/RechercheVilleTest.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Exercice4TDD;
 
 namespace Exercice4TDD.MsTest
 {
     [TestClass]
     internal class RechercheVilleTest
     {
-        
+        private RechercheVille? _rechercheVille;
+
+        [TestInitialize] // s'exécutera AVANT CHAQUE TEST
+        public void SetUp()
+        {
+            _rechercheVille = new RechercheVille();
+        }
+        [TestCleanup]  // s'exécutera APRES CHAQUE TEST
+        public void Down()
+        {
+            _rechercheVille = null;
+        }
+
+        // 1 - Si le texte de la recherche contient moins de 2 caractères, Une exception est levée de type NotFoundException.
+        [TestMethod]
+        public void WhenRechercheVille_LessThan2Digits_Then_NotFoundException()
+        {
+        }
     }
 }
